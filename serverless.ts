27d29fff8538c210eab3b89d7984245f6a028528
Aditya <xditya@ultroid.tech>
2022-11-08 11:39:51 +0530

    feat: filter webhooks

--- serverless.ts
+++ serverless.ts
@@ -2,4 +2,18 @@ import { serve } from "https://deno.land/std@0.154.0/http/server.ts";
 import { webhookCallback } from "https://deno.land/x/grammy@v1.10.1/mod.ts";
 import { bot } from "./bot.ts";
 
-serve(webhookCallback(bot, "std/http"));
+const handleUpdate = webhookCallback(bot, "std/http");
+
+serve(async (req) => {
+    if (req.method === "POST") {
+        const url = new URL(req.url);
+        if (url.pathname.slice(1) === bot.token) {
+            try {
+                return await handleUpdate(req);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+    return new Response();
+});
